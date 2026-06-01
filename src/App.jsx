2e1860bb115f4ac46db3e9253b73a137e33a58a6
Laboratorio 320 <AFT_LAB320@inacap.cl>
2026-06-01 09:34:14 -0400

    crear campo para escribir

--- src/App.jsx
+++ src/App.jsx
@@ -7,10 +7,21 @@ function App() {
     const [texto, setTexto] = useState("");
 
     return (
-      // ...
+      <div className="app">
+        <h1>Mi lista de tareas</h1>
+
+        <div className="entrada">
+          <input
+            value={texto}
+            onChange={(e) => setTexto(e.target.value)}
+            placeholder="Escribe una tarea"
+            />
+            <button>Agregar</button>
+        </div>
+      </div>
     );
   }
-  
+
   return (
     <div className="app">
       <h1>Mi lista de tareas</h1> 
