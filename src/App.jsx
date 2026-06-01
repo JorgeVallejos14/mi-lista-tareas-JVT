0922288939cb16096f7cfb23bd2c6622b23bcac5
Laboratorio 320 <AFT_LAB320@inacap.cl>
2026-06-01 10:01:50 -0400

    marcar y eliminar tareas

--- src/App.jsx
+++ src/App.jsx
@@ -2,9 +2,32 @@ import { useState } from "react";
 import "./App.css";
 
 function App() {
-  function App() {
     const [tareas, setTareas] = useState([]);
     const [texto, setTexto] = useState("");
+
+    function agregarTarea() {
+      if (texto.trim() === "") return;
+      const nueva = {
+        id: Date.now(),
+        titulo: texto,
+        hecha: false,
+      };
+      setTareas([...tareas, nueva]);
+      setTexto("");
+    }
+
+    function alternarTarea(id) {
+      setTareas(
+        tareas.map((t) =>
+          t.id === id ? { ...t, hecha: !t.hecha } : t
+        )
+      );
+    }
+
+    function eliminarTarea(id) {
+      setTareas(tareas.filter((t) => t.id !== id));
+    }
+
 
     return (
       <div className="app">
@@ -24,7 +47,32 @@ function App() {
 
   return (
     <div className="app">
-      <h1>Mi lista de tareas</h1> 
+      <h1>Mi lista de tareas</h1>
+
+      <div className="entrada">
+        <input
+          value={texto}
+          onChange={(e) => setTexto(e.target.value)}
+          placeholder="Escribe una tarea"
+          />
+          <button onClick={agegarTarea}>Agregar</button>
+        </div>
+
+        <ul>
+          {tareas.map((t) => (
+            <li key={t.id}>
+              <span
+                onClick={() => alternarTarea(t.id)}
+                className={t.hecha ? "hecha" : ""}
+              >
+                {t.titulo}
+                </span>
+                <button onClick={() => eliminarTarea(t.id)}>X</button>
+            </li>
+          ))}
+        </ul>
+
+        <p>{tareas.lenght} tarea(s) en total</p>
     </div>
   );
 }
