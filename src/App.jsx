8625e5a4df475c32d12739babfe70e8a698d52bc
Laboratorio 320 <AFT_LAB320@inacap.cl>
2026-06-01 09:28:55 -0400

    primer commit

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+import { useState } from "react";
+import "./App.css";
+
+function App() {
+  function App() {
+    const [tareas, setTareas] = useState([]);
+    const [texto, setTexto] = useState("");
+
+    return (
+      // ...
+    );
+  }
+  
+  return (
+    <div className="app">
+      <h1>Mi lista de tareas</h1> 
+    </div>
+  );
+}
+
+export default App;
